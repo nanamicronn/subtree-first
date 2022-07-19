e632c9b04be0d83e98ae73d562be73df70bb303c
NanamiTanida <nanami_tanida@rebuild-inc.co.jp>
2022-07-19 15:33:46 +0900

    [fix] ミスったsubtree削除

--- components/.prettierrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  useTabs: false,
-  semi: false,
-  singleQuote: true,
-  jsxSingleQuote: false,
-  trailingComma: "all",
-  bracketSpacing: true,
-};
--- components/src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from 'next'
-
-const Index: NextPage = () => {
-  return <div>Subtree First</div>
-}
-
-export default Index
