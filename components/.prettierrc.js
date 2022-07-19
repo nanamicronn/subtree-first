f6342ff170ea9cf5d5ad1f782e93dcfcbde9cc07
NanamiTanida <nanami_tanida@rebuild-inc.co.jp>
2022-07-19 15:30:40 +0900

    Add 'components/' from commit '8f82c859a4d94e7ff99b76a98a31cf9388bd7940'
    git-subtree-dir: components
    git-subtree-mainline: 8f82c859a4d94e7ff99b76a98a31cf9388bd7940
    git-subtree-split: 8f82c859a4d94e7ff99b76a98a31cf9388bd7940

---
+++ components/.prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  trailingComma: "all",
+  bracketSpacing: true,
+};
---
+++ components/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next'
+
+const Index: NextPage = () => {
+  return <div>Subtree First</div>
+}
+
+export default Index
